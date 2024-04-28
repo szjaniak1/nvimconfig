f388db41d9aa104b35b35104f2f5161060b14b20
szjaniak <szjaniak1@gmail.com>
2024-04-28 12:55:55 +0200

    added golang lsp server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -15,6 +15,7 @@ lsp_zero.on_attach(function(client, bufnr)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+require('lspconfig').gopls.setup({})
 require('lspconfig').pyright.setup({})
 require('lspconfig').clangd.setup({})
 
